c0f7e3c72dcebe5275ba69d8def238ad2822eb5a
Trinidad <trinidadhuerta1@gmail.com>
2020-06-30 12:39:57 -0500

    linked app to index template

--- Revelius/revelius.py
+++ Revelius/revelius.py
@@ -1,14 +1,21 @@
 """
 Module Docstring
 """
+
 __author__ = "Trinidad Huerta"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'thisisasecret' #this needs to be actually a random key
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
